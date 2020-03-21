63bb55fcd10ba7fa5a8297d6369028707eb14eea
Poznyakovskiy <apoznyakovskiy@deloitte.de>
2020-03-21 18:22:34 +0100

    Fixed user fetching.

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -2,10 +2,12 @@ import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Store} from '@ngxs/store';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
 
 import {AuthService, CoreService} from '../../services';
 import { UserAction } from 'src/app/store';
-
+import { User } from 'src/app/models';
 
 @Component({
   selector: 'app-login',
@@ -22,7 +24,8 @@ export class LoginPage {
       private router: Router,
       private fb: FormBuilder,
       private coreService: CoreService,
-      private store: Store
+      private store: Store,
+      private db: AngularFirestore
   ) {
     this.createForm();
   }
@@ -37,7 +40,11 @@ export class LoginPage {
   tryLogin() {
     this.authService.doLogin(this.loginForm.value)
         .then(res => {
-          this.store.dispatch(new UserAction.Set(res.user));
+          this.db.collection('user').doc(res.user.uid).valueChanges().pipe(
+            take(1)
+          ).subscribe((user: User) => {
+            this.store.dispatch(new UserAction.Set(user));
+          });
           this.router.navigate(['/home']);
         }, err => {
           console.log(err);
@@ -73,7 +80,11 @@ export class LoginPage {
     } else {
       this.authService.setLastLogin(res);
     }
-    this.store.dispatch(new UserAction.Set(res.user));
+    this.db.collection('user').doc(res.user.uid).valueChanges().pipe(
+      take(1)
+    ).subscribe((user: User) => {
+      this.store.dispatch(new UserAction.Set(user));
+    });
     this.coreService.showToastWithButton(
         `Welcome, ${res.user.displayName}`,
         'success'
