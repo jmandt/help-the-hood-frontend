e0028304e7c83361d62ff87f18c132b28eef1205
Jonathan Mandt <jonathan.mandt@gmail.com>
2020-03-22 11:43:13 +0100

    data loaded after user is in store

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,20 +1,28 @@
 import {Component, OnInit} from '@angular/core';
 import {JobsService} from '../services/jobs';
+import {Select} from '@ngxs/store';
+import {UserState} from '../store/state';
+import {Observable} from 'rxjs';
+import {User} from '../models';
 
 @Component({
     selector: 'app-tab2',
     templateUrl: 'tab2.page.html',
     styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page {
 
     jobsTaken;
     jobsPosted;
 
+    @Select(UserState) user$: Observable<User>;
+
+
     constructor(private jobService: JobsService) {
+        this.user$.subscribe(user => user.uid ? this.init() : undefined);
     }
 
-    ngOnInit() {
+    init() {
         this.jobService.getAllCommittedJobsByUser().subscribe(jobs => {
             this.jobsTaken = jobs;
         });
